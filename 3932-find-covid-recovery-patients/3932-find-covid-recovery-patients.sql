051bb0e2e19299594be452605b64b5a2269cc5ee
Hyeonjin Kim <161192870+andrew75313@users.noreply.github.com>
2025-07-10 22:49:23 +0900

    Time: 576 ms (22.41%), Space: 0B (100%) - LeetHub

--- 3932-find-covid-recovery-patients/3932-find-covid-recovery-patients.sql
+++ 3932-find-covid-recovery-patients/3932-find-covid-recovery-patients.sql
@@ -1,31 +1,23 @@
-WITH first_contam AS (
+SELECT
+    p.patient_id,
+    p.patient_name,
+    p.age,
+    DATEDIFF(
+        MIN(CASE WHEN ct.result = 'Negative' AND ct.test_date > first_pos.first_contam_date THEN ct.test_date END),
+        first_pos.first_contam_date
+    ) AS recovery_time
+FROM patients p
+JOIN (
     SELECT
         patient_id,
         MIN(test_date) AS first_contam_date
     FROM covid_tests
     WHERE result = 'Positive'
-    GROUP BY 1
-),
-first_cured AS (
-    SELECT
-        CT.patient_id,
-        MIN(CT.test_date) AS first_cured_date    
-    FROM covid_tests CT
-    JOIN first_contam FC
-    ON CT.patient_id = FC.patient_id
-    WHERE CT.result = 'Negative'
-        AND CT.test_date > FC.first_contam_date 
-    GROUP BY 1
-)
-
-SELECT
-    CON.patient_id,
-    P.patient_name,
-    P.age,
-    DATEDIFF(CUR.first_cured_date, CON.first_contam_date) AS recovery_time 
-FROM first_contam CON
-JOIN first_cured CUR
-ON CON.patient_id = CUR.patient_id
-JOIN patients P
-ON CON.patient_id = P.patient_id
-ORDER BY 4 ASC, 2 ASC
+    GROUP BY patient_id
+) AS first_pos
+  ON p.patient_id = first_pos.patient_id
+JOIN covid_tests ct
+  ON ct.patient_id = p.patient_id
+GROUP BY p.patient_id, p.patient_name, p.age, first_pos.first_contam_date
+HAVING recovery_time IS NOT NULL
+ORDER BY recovery_time ASC, p.patient_name ASC;
