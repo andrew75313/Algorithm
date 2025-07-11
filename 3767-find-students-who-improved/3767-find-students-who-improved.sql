8d161cd6e8f4f80ba3cacd8f477b7f5aafd3ea05
Hyeonjin Kim <161192870+andrew75313@users.noreply.github.com>
2025-07-11 12:06:34 +0900

    Time: 322 ms (23.91%), Space: 0B (100%) - LeetHub

--- 3767-find-students-who-improved/3767-find-students-who-improved.sql
+++ 3767-find-students-who-improved/3767-find-students-who-improved.sql
@@ -1,11 +1,21 @@
-WITH total_scores AS (
+WITH more_exam AS (
+    SELECT
+        student_id,
+        subject,
+        COUNT(*) AS cnt
+    FROM Scores
+    GROUP BY 1, 2
+    HAVING cnt >= 2
+),
+total_scores AS (
     SELECT DISTINCT
         student_id,
         subject,
         FIRST_VALUE(score) OVER (PARTITION BY student_id, subject ORDER BY exam_date ASC) AS first_score,
-        FIRST_VALUE(score) OVER (PARTITION BY student_id, subject ORDER BY exam_date DESC) AS latest_score,
-        COUNT(*) OVER (PARTITION BY student_id, subject) AS cnt
-    FROM Scores    
+        FIRST_VALUE(score) OVER (PARTITION BY student_id, subject ORDER BY exam_date DESC) AS latest_score
+    FROM Scores
+    WHERE (student_id, subject) IN (SELECT student_id, subject FROM more_exam)
+    
 )
 
 SELECT
@@ -15,5 +25,4 @@ SELECT
     latest_score
 FROM total_scores
 WHERE first_score < latest_score
-    AND cnt >= 2
-ORDER BY 1 ASC, 2 ASC
+ORDER BY 1 ASC, 2
