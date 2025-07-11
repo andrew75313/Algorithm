1aada07ea9681bbb0758e51ed62731435bffd81d
Hyeonjin Kim <161192870+andrew75313@users.noreply.github.com>
2025-07-11 12:00:24 +0900

    Time: 324 ms (22.63%), Space: 0B (100%) - LeetHub

---
+++ 3767-find-students-who-improved/3767-find-students-who-improved.sql
@@ -0,0 +1,28 @@
+WITH more_exam AS (
+    SELECT
+        student_id,
+        subject,
+        COUNT(*) AS cnt
+    FROM Scores
+    GROUP BY 1, 2
+    HAVING cnt >= 2
+),
+total_scores AS (
+    SELECT DISTINCT
+        student_id,
+        subject,
+        FIRST_VALUE(score) OVER (PARTITION BY student_id, subject ORDER BY exam_date ASC) AS first_score,
+        FIRST_VALUE(score) OVER (PARTITION BY student_id, subject ORDER BY exam_date DESC) AS latest_score
+    FROM Scores
+    WHERE (student_id, subject) IN (SELECT student_id, subject FROM more_exam)
+    
+)
+
+SELECT
+    student_id,
+    subject,
+    first_score,
+    latest_score
+FROM total_scores
+WHERE first_score < latest_score
+ORDER BY 1 ASC, 2 ASC
