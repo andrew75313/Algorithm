0048c29d37265fa6999eb20349c7a4af1d73211f
Hyeonjin Kim <161192870+andrew75313@users.noreply.github.com>
2025-07-15 21:50:19 +0900

    Time: 623 ms (17.83%), Space: 0B (100%) - LeetHub

---
+++ 3942-find-drivers-with-improved-fuel-efficiency/3942-find-drivers-with-improved-fuel-efficiency.sql
@@ -0,0 +1,40 @@
+WITH first_half AS (
+    SELECT *
+    FROM trips
+    WHERE MONTH(trip_date) >= 1 AND MONTH(trip_date) <= 6
+),
+second_half AS (
+    SELECT *
+    FROM trips
+    WHERE MONTH(trip_date) >= 7 AND MONTH(trip_date) <= 12
+),
+valid_user_average AS (
+    SELECT
+        driver_id,
+        CASE
+            WHEN (MONTH(trip_date) >= 1 AND MONTH(trip_date) <= 6) THEN 'first'
+        ELSE 'second' 
+        END AS half,
+        distance_km / fuel_consumed AS fuel_efficiency
+    FROM trips
+    WHERE driver_id IN (SELECT driver_id FROM first_half) AND driver_id IN (SELECT driver_id FROM second_half)
+),
+valid_user_total_average AS (
+    SELECT
+        driver_id,
+        AVG(CASE WHEN half = 'first' THEN fuel_efficiency ELSE NULL END) AS first_half_avg,
+        AVG(CASE WHEN half = 'second' THEN fuel_efficiency ELSE NULL END) AS second_half_avg
+    FROM valid_user_average
+    GROUP BY 1
+)
+
+SELECT
+    V.driver_id,
+    D.driver_name,
+    ROUND(V.first_half_avg, 2) AS first_half_avg,
+    ROUND(V.second_half_avg, 2) AS second_half_avg,
+    ROUND(V.second_half_avg - V.first_half_avg, 2) AS efficiency_improvement
+FROM valid_user_total_average V
+JOIN drivers D ON V.driver_id = D.driver_id
+WHERE first_half_avg < second_half_avg
+ORDER BY 5 DESC, 2 ASC
