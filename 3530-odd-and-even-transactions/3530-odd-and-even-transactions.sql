14dcd1ce971be93432688c25eaa920447ed901ff
Hyeonjin Kim <161192870+andrew75313@users.noreply.github.com>
2025-07-22 16:06:53 +0900

    Time: 268 ms (81.96%), Space: 0B (100%) - LeetHub

--- 3530-odd-and-even-transactions/3530-odd-and-even-transactions.sql
+++ 3530-odd-and-even-transactions/3530-odd-and-even-transactions.sql
@@ -1,17 +1,17 @@
-WITH number_type AS (
-    SELECT
+
+
+SELECT
+    transaction_date,
+    SUM(CASE WHEN type = 'odd' THEN amount ELSE 0 END) AS odd_sum,
+    SUM(CASE WHEN type = 'even' THEN amount ELSE 0 END) AS even_sum
+FROM (
+        SELECT
         *,
         CASE
             WHEN (amount % 2) = 0 THEN 'even'
             ELSE 'odd'
         END AS type
     FROM transactions
-)
-
-SELECT
-    transaction_date,
-    SUM(CASE WHEN type = 'odd' THEN amount ELSE 0 END) AS odd_sum,
-    SUM(CASE WHEN type = 'even' THEN amount ELSE 0 END) AS even_sum
-FROM number_type
+) SUB
 GROUP BY transaction_date
 ORDER BY 1 ASC
